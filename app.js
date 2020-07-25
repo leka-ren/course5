49af580221d3a96a9a7da60bc8e1a1592c574f64
leka-ren <polafort@gmail.com>
2020-07-25 16:27:13 +0300

    добавлена валидция Joi для sign IN & UP

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ const { json } = require('body-parser');
 
 const mongoose = require('mongoose');
 
+const { celebrate, Joi, errors } = require('celebrate');
+
 const { login, createUser } = require('./controllers/users');
 
 const app = express();
 
 const auth = require('./middlewares/auth');
+
+const regexUrl = require('./regExp/urlValid');
 
 const { PORT = 3000 } = process.env;
 const baseUrl = 'mongodb://localhost:27017/mestodb';
@@ -22,8 +26,30 @@ mongoose.connect(baseUrl, {
   useUnifiedTopology: true,
 });
 
-app.post('/signin', login);
-app.post('/signup', createUser);
+app.post(
+  '/signin',
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required().email(),
+      password: Joi.string().required().min(8),
+    }),
+  }),
+  login,
+);
+
+app.post(
+  '/signup',
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required().min(2).max(30),
+      about: Joi.string().required().min(2).max(30),
+      avatar: Joi.string().required().regex(regexUrl),
+      email: Joi.string().required().email(),
+      password: Joi.string().required().min(8),
+    }),
+  }),
+  createUser,
+);
 
 app.use(auth);
 
@@ -35,6 +61,8 @@ app.use((req, res, next) => {
   err.statusCode = 404;
   next(err);
 });
+
+app.use(errors());
 
 app.use((err, req, res) => {
   res.status(err.statusCode).send(err);
