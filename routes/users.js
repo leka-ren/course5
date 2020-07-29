74cc13d25da2e8a2e9dab8c85cb3fb159f59fdbf
leka-ren <polafort@gmail.com>
2020-07-29 11:13:18 +0300

    фикс валдиации hex для id

--- routes/users.js
+++ routes/users.js
@@ -14,7 +14,7 @@ router.get(
   '/:id',
   celebrate({
     params: Joi.object().keys({
-      id: Joi.string().alphanum().length(24),
+      id: Joi.string().hex().length(24),
     }),
   }),
   getUsersById,
